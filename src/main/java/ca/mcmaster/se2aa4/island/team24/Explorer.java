da0eac34500064dd32ac1a790339930bca05f89b
DYousuf05 <danyalyousuf05@gmail.com>
2025-03-21 19:36:40 -0400

    Added distance between site and creek calculation

--- src/main/java/ca/mcmaster/se2aa4/island/team24/Explorer.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team24/Explorer.java
@@ -10,10 +10,9 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 
-class DetectLand {
-    // Use scanner and radar to analyze oncoming land masses
-    // Determines whether to stop or fly the drone
-    // Holds a parameter to indicate whether to search for creek or emergency site
+class LandCell {
+    public int x = 0;
+    public int y = 0;
 }
 
 class Flight {
@@ -28,12 +27,18 @@ enum Rotate {
 public class Explorer implements IExplorerRaid {
 
     private final Logger logger = LogManager.getLogger();
+    
     private boolean hasEchoed = false; // If the drone has used 'ECHO'
     private boolean hasScanned = false; // If the drone has used 'SCAN'
     private boolean foundCreek = false; // If the drone has scanned a creek
     private boolean foundSite = false; // If the drone has scanned an emergency site
+    
+    private LandCell currentPos;
+    private LandCell creekPos;
+    private LandCell sitePos;
+
     private int currentBattery = 0;
-    private int count = 0;
+    private double finalDistance = 0.0;
     private int range = 0; // Max range between drone and out-of-range border
     private int distanceToEnd = 0; // Variable to check the distance between the drone and the grid border
     private Rotate nextRotation = Rotate.CW; // How to turn depending on direction of travel
@@ -43,6 +48,11 @@ public class Explorer implements IExplorerRaid {
 
     @Override
     public void initialize(String s) {
+        currentPos = new LandCell();
+        creekPos = new LandCell();
+        sitePos = new LandCell();
+        currentPos.x = 1;
+        currentPos.y = 1;
         logger.info("** Initializing the Exploration Command Center");
         JSONObject info = new JSONObject(new JSONTokener(new StringReader(s)));
         logger.info("** Initialization info:\n {}",info.toString(2));
@@ -58,23 +68,29 @@ public class Explorer implements IExplorerRaid {
         JSONObject decision = new JSONObject(); // For the action
         JSONObject parameters = new JSONObject(); // For any actions that include parameters 
         
-        if (currentBattery <= 10) {
-            decision.put("action", "stop");
-        }
-
         if (creeks.length() > 0 && !foundCreek) {
             foundCreek = true;
+            creekPos.x = currentPos.x; 
+            creekPos.y = currentPos.y; 
         }
 
         if (emergencySites.length() > 0 && !foundSite) {
             foundSite = true;
+            sitePos.x = currentPos.x; 
+            sitePos.y = currentPos.y; 
         }
 
-        if (!foundCreek || !foundSite) {
+        if ((!foundCreek || !foundSite) && currentBattery > 100) {
             if (hasEchoed && hasScanned) { // Checks if 'ECHO' and 'SCAN' have been executed
                 if (distanceToEnd > 1) {
                     decision.put("action", "fly"); // Straight movement
                     distanceToEnd--;
+                    if (dir.equals("E")) {
+                        currentPos.x++;
+                    }
+                    else {
+                        currentPos.x--;
+                    }
                 }
                 else {
                     decision.put("action", "heading");
@@ -82,24 +98,28 @@ public class Explorer implements IExplorerRaid {
                         if (dir.equals("E")) {
                             parameters.put("direction", "S");
                             dir = "S";
+                            currentPos.y++;
                         }
                         else if (dir.equals("S")) {
                             parameters.put("direction", "W");
                             dir = "W";
                             nextRotation = Rotate.CCW;
                             distanceToEnd = range - 1;
+                            currentPos.x--;
                         }
                     }
                     else { // Counter-clockwise Turn (West to East)
                         if (dir.equals("W")) {
                             parameters.put("direction", "S");
                             dir = "S";
+                            currentPos.y++;
                         }
                         else if (dir.equals("S")) {
                             parameters.put("direction", "E");
                             dir = "E";
                             nextRotation = Rotate.CW;
                             distanceToEnd = range - 1;
+                            currentPos.x++;
                         }
                     }
                     decision.put("parameters", parameters);
@@ -118,9 +138,9 @@ public class Explorer implements IExplorerRaid {
                     hasScanned = true;
                 }
             }
-            count++;
         }
         else {
+            finalDistance = Math.sqrt(Math.pow(sitePos.x - creekPos.x, 2) + Math.pow(sitePos.y - creekPos.y, 2));
             decision.put("action", "stop");
         }
         logger.info("** Decision: {}",decision.toString());
@@ -144,7 +164,9 @@ public class Explorer implements IExplorerRaid {
             distanceToEnd = range;
         }
         if (extraInfo.has("creeks")) {
-            creeks = extraInfo.getJSONArray("creeks");
+            if (creeks.length() == 0) {
+                creeks = extraInfo.getJSONArray("creeks");
+            }
         }
         if (extraInfo.has("sites")) {
             emergencySites = extraInfo.getJSONArray("sites");
@@ -153,7 +175,30 @@ public class Explorer implements IExplorerRaid {
 
     @Override
     public String deliverFinalReport() {
-        return "no creek found";
+        logger.info("***** FINAL REPORT *****");
+
+        if (creeks.length() > 0 && emergencySites.length() > 0) {
+            logger.info("Final Status: Creek and Emergency Site located. Distance calculated.");
+            logger.info("The distance between the 2 sites is: " + finalDistance);
+        }
+        else {
+            logger.info("Final Status: Drone battery at critical level, returned to home to prevent MIA.");
+            logger.info("The distance between the 2 sites is: N/A");
+        }
+
+        if (creeks.length() > 0) {
+            logger.info("The located creek identifier is: " + creeks.getString(0));
+        } else {
+            logger.info("No creek found.");
+        }
+        
+        if (emergencySites.length() > 0) {
+            logger.info("The located emergency site identifier is: " + emergencySites.getString(0));
+        } else {
+            logger.info("No emergency site found.");
+        }
+       
+        return "report printed";
     }
 
 }
